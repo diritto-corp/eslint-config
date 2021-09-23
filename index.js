fb230fb4ddf684ab38ce95d93618fb480f4f8551
eoomttt <hyunt0413@gmail.com>
2021-09-23 20:04:20 +0900

    Spell checker plugin 추가

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
+  plugins: ['spellcheck'],
   extends: ['eslint:recommended', 'airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
@@ -11,5 +12,16 @@ module.exports = {
   rules: {
     // default export 안해도 okay
     'import/prefer-default-export': 'off',
+    // spell check for eslint-config
+    'spellcheck/spell-checker': ['error', {
+      lang: 'en_US',
+      comments: false,
+      strings: false,
+      skipWords: [
+        'lang',
+        'jsx',
+        'ecma',
+      ],
+    }],
   },
 };
